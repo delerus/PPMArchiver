786a6692a42d85aedac84f60c11fd4f30cbe4a7d
delerus <delerus21@gmail.com>
2025-04-28 03:06:36 +0300

    applied unsaved changes to the interfaces

--- Core/Contexts/IContextModel.cs
+++ Core/Contexts/IContextModel.cs
@@ -8,6 +8,15 @@ namespace PPMdArchiver.Core.Contexts
 {
     internal interface IContextModel
     {
+        void Initialize(int order, int memoryMb);
+        void Reset();
+        // TODO: Define more functions for context model interface
+    }
 
+    public readonly struct SymbolProbability
+    {
+        public byte Symbol { get; }
+        public uint Frequency { get; }
+        public uint CumulativeFrequency { get; }
     }
 }
--- Core/IO/IIOHandler.cs
+++ Core/IO/IIOHandler.cs
@@ -2,8 +2,16 @@
 
 namespace PPMdArchiver.Core.IO
 {
+    /// <summary>
+    /// General IO interface for working with files and streams
+    /// </summary>
     internal interface IIOHandler
     {
-
+        void OpenInFile(string inputFile);
+        void OpenOutFile(string outputFile);
+        bool ReadBlock(byte[] buffer, out int bytesRead);
+        bool WriteBlock(byte[] buffer, int length);
+        void Close();
+        bool IsEof { get; }
     }
 }
