71bc5bd9f8d8a9a2923455bf55c0367fc2ca3656
delerus <delerus21@gmail.com>
2025-05-03 20:57:31 +0300

    added safety to BitWriter class and added method WriteBis

--- Core/Utils/BitWriter.cs
+++ Core/Utils/BitWriter.cs
@@ -11,14 +11,20 @@ namespace PPMdArchiver.Core.Utils
         private readonly Stream _stream;
         private byte _buffer = 0;
         private int _bitsInBuffer = 0;
+        private bool _disposed = false;
 
         public BitWriter(Stream stream)
         {
+            if (stream == null || !stream.CanWrite)
+                throw new ArgumentException("Stream must be writable.");
             _stream = stream;
         }
 
         public void WriteBit(byte bit)
         {
+            if (_disposed)
+                throw new ObjectDisposedException(nameof(BitWriter));
+
             _buffer = (byte)(_buffer << 1);
             _buffer |= (byte)(bit & 1);
             _bitsInBuffer++;
@@ -31,8 +37,39 @@ namespace PPMdArchiver.Core.Utils
             }
         }
 
+        private void WriteBits(uint bits, int bitCount)
+        {
+            if (_disposed)
+                throw new ObjectDisposedException(nameof(BitWriter));
+
+            if (bitCount < 0 || bitCount > 32)
+                throw new ArgumentOutOfRangeException(nameof(bitCount), "bitCount must be between 0 and 32.");
+
+            if (bitCount == 0)
+                return;
+
+            for (int i = bitCount - 1; i >= 0; i--)
+            {
+                byte bit = (byte)((bits >> i) & 1);
+
+                _buffer = (byte)(_buffer << 1);
+                _buffer |= bit;
+                _bitsInBuffer++;
+
+                if (_bitsInBuffer == 8)
+                {
+                    _stream.WriteByte(_buffer);
+                    _buffer = 0;
+                    _bitsInBuffer = 0;
+                }
+            }
+        }
+
         public void Flush()
         {
+            if (_disposed)
+                throw new ObjectDisposedException(nameof(BitWriter));
+
             if (_bitsInBuffer > 0)
             {
                 _buffer <<= 8 - _bitsInBuffer;
@@ -42,6 +79,13 @@ namespace PPMdArchiver.Core.Utils
             }
         }
 
-        public void Dispose() => Flush();
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                Flush();
+                _disposed = true;
+            }
+        }
     }
 }
